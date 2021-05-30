60da2936560f5c4e119b617553ecf528839e8aa7
aditya324jhj <79002262+aditya324jhj@users.noreply.github.com>
2021-05-30 09:16:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -17,12 +17,12 @@ document.getElementById("display_menu").innerHTML = htmldata;
 function add_item(){
 var htmldata;
 var item=document.getElementById("add_item").value;
-menu_list_array.push(item)
+
 menu_list_array.sort();
 htmldata="<section class='cards'>"
 for(var i=0; i<menu_list_array.length;i++){
 htmldata = htmldata+'<div class = "card">'
-+'<img src = "pizzaImg.png">'
++'<img src = "images/pizzaImg.png"/>'
 + menu_list_array[i] + '</div>'
 
 
